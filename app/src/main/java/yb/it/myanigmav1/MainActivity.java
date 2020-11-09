c9af65629f1fb7b0f37a3b3c2cb53374ec0c9746
Yousef Ballan <ballan_yousef@yahoo.com>
2020-11-09 20:05:59 +0200

    Initial commit

---
+++ app/src/main/java/yb/it/myanigmav1/MainActivity.java
@@ -0,0 +1,15 @@
+package yb.it.myanigmav1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hi Enigma
+    }
+}
